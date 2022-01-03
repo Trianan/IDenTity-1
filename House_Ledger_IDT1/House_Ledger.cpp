ac2bc1154899d633148d068e2bd9349484c7d993
Trianan <78253276+Trianan@users.noreply.github.com>
2022-01-02 23:44:57 -0500

    Add files via upload

--- House_Ledger_IDT1/House_Ledger.cpp
+++ House_Ledger_IDT1/House_Ledger.cpp
@@ -31,8 +31,10 @@
 
 							- hi tristin (Jan 1/2022 - Ash)
 
-							
+							- Implement saving to files for BalanceTable date, and automatically loading the 
+							  last logged-in BalanceTable upon startup of UI, if there was one. (Jan 2/2022 - Trianan)
 
+						
 	<------ -= ======------ -= ======------ -= ======-------> CHANGELOG <------ -= ======------ -= ======------ -= ======------->
 
 							- Verification that a buyer is a resident when adding a household expense implemented. (Dec 26/2021 - Trianan)
@@ -44,13 +46,13 @@
 							- Major refactoring. (Dec 30/2021 - Trianan)
 
 							- Gutted program almost entirely and restarted with BalanceTable concept and 
-							  different stucture in general. (Dec 31/2021 - Trianan)
+							  different stucture in general. House and associated code removed. (Dec 31/2021 - Trianan)
 
 							- Gutted program again with different structure. (Jan 1/2021 - Trianan)
 
-							- Fleshed out the Account and BalanceTable classes; decided to use more
-							  bool-returning functions where I'd normally use void, as this will simplify
-							  programming the UI. (Jan 2/2021 - Trianan) 
+							- Fleshed out the Account and BalanceTable classes; more
+							  bool-returning functions used for simplifying the UI implementation.
+							  Started development on UI. (Jan 2/2021 - Trianan) 
 */
 
 // < ------ -= ======------ -= ======------> P R E P R O C E S S O R <------ -= ======------ -= ======------->
@@ -83,12 +85,14 @@ hash<string> hash_str;
 
 int account_id_length = 8;
 int ascii_table_length = 127;
+string default_password = "default";
+string default_ui_save = "ui_save.txt";
 
 
 /* <------ -= ======------ -= ======------> C L A S S E S <------ -= ======------ -= ======------->
 	  * * * * + + + + * * * * + + + + * * * * + + <O> * * + + + + * * * * + + + + * * * * + + + + 
 	  
-// <------ -= ======------ -= ======------> ACCOUNT < ------ -= ======------ -= ======-------> */
+   <------ -= ======------ -= ======------> ACCOUNT < ------ -= ======------ -= ======-------> */
 class Account {
 	// Represents an individual.
 public:
@@ -134,7 +138,26 @@ bool Account::operator== (Account& other) {
 		return true;
 	else return false;
 }
-
+// Core functions:
+bool Account::add_balance_holder(Account& holder) {
+	if (!existing_holder(holder)) {
+		Balance new_balance{ holder, 0.0 };
+		balances.push_back(new_balance);
+		return true;
+	}
+	return false;
+}
+bool Account::change_balance(Account& holder, double amount) {
+	if (existing_holder(holder)) {
+		for (Balance existing : balances)
+			if (existing.holder == holder) {
+				existing.amount += amount;
+				return true;
+			}
+		return false;
+	}
+}
+// Helpers:
 bool Account::change_password(string old_pswd, string new_pswd) {
 	// Changes the password if the old password given is correct:
 	if (is_password(old_pswd)) {
@@ -157,39 +180,20 @@ bool Account::existing_holder(Account& holder) {
 			return true;
 	return false;
 }
-bool Account::add_balance_holder(Account& holder) {
-	if (!existing_holder(holder)) {
-		Balance new_balance{ holder, 0.0 };
-		balances.push_back(new_balance);
-		return true;
-	}
-	return false;
-}
-bool Account::change_balance(Account& holder, double amount) {
-	if (existing_holder) {
-		for (Balance existing : balances)
-			if (existing.holder == holder) {
-				existing.amount += amount;
-				return true;
-			}
-		return false;
-	}
-}
 
 
 // <------ -= ======------ -= ======------> TRANSACTION < ------ -= ======------ -= ======------->
 struct Transaction {
+	// A record of a transaction between two accounts.
 	Transaction(Account& a, Account& b, double amnt, bool shrd=false);
-	Account& from;
-	Account& to;
-	double amount;
-	string date;
-	bool shared;
+	const Account& from;
+	const Account& to;
+	const double amount;
+	const string date = get_current_date();
+	const bool shared;
 };
 Transaction::Transaction(Account& a, Account& b, double amnt, bool shrd)
-	: from(a), to(b), amount(amnt), shared(shrd) {
-	date = get_current_date();
-}
+	: from(a), to(b), amount(amnt), shared(shrd) {}
 
 
 // <------ -= ======------ -= ======------> BALANCE < ------ -= ======------ -= ======------->
@@ -208,24 +212,70 @@ Balance::Balance(Account& hldr, double amnt)
 class BalanceTable {
 	// Represents the balances between all participents.
 public:
-	BalanceTable(string filename);
-	BalanceTable(string filename, vector<Account&> starting_accounts);
+	BalanceTable(string nm, string filename);
+	BalanceTable(string nm, string filename, vector<Account&> starting_accounts);
 	bool existing_account(Account& account);
 	bool add_account(Account& account);
 	bool modify_balance_entry(Account& account, Account& holder, double amount);
 	bool create_transaction(Account& from, Account& to, double amount, bool shared=false);
-	bool dist_cost(Account& buyer, double amount, vector<Account&> omissions = vector<Account&>{});
+	bool distribute_cost(Account& buyer, double amount, vector<Account&> omissions = vector<Account&>{});
+	void record_shared_transaction(Transaction t) { shared_transaction_history.push_back(t); }
 
 private:
+	string name; // Name of group on table.
 	string savefile; // File used to save and pull data from.
 	vector<Account&> accounts; // References to each associated account.
 
-};
-BalanceTable::BalanceTable(string filename)
-	:savefile(filename) {};
-BalanceTable::BalanceTable(string filename, vector<Account&> starting_accounts)
-	:savefile(filename), accounts(starting_accounts) {};
+	Account table_account;
+	vector<Transaction> shared_transaction_history;
 
+};
+BalanceTable::BalanceTable(string nm, string filename)
+	: name(nm), savefile(filename), table_account(Account{ nm,default_password }) {};
+BalanceTable::BalanceTable(string nm, string filename, vector<Account&> starting_accounts)
+	: name(nm), savefile(filename), table_account(Account{ nm,default_password }), accounts(starting_accounts) {};
+// Core functions:
+bool BalanceTable::create_transaction(Account& a, Account& b, double amount, bool shared) {
+	/* Modifies the correct entriesand generates a transaction entry between two accounts:
+		- Positive amounts are credits owed to 'a' by 'b'.
+		- Negative amounts are debts owed to 'b' from 'a'.
+		- Credits for 'a' correspond to debts from 'b', and vica-versa.
+	*/
+	if (existing_account(a) && existing_account(b)) {
+		// From 'a' to 'b':
+		modify_balance_entry(a, b, amount);
+		a.record_transaction(Transaction{ a,b,amount,shared });
+		// From 'b' to 'a':
+		modify_balance_entry(b, a, -amount);
+		b.record_transaction(Transaction{ b,a,-amount,shared });
+		return true;
+	}
+	return false;
+}
+bool BalanceTable::distribute_cost(Account& buyer, double amount, vector<Account&> excluded_accounts) {
+	/* Distributes a given cost or credit evenly amongst sharers:
+		- Generates individual transactions between buyer and sharers.
+		- Cost for each sharer is calculated by (net cost)/(# of sharers).
+		- Transactions are marked as shared costs/credits.
+	*/
+	if (existing_account(buyer)) {
+		double individual_cost = amount / (accounts.size() - excluded_accounts.size());
+		excluded_accounts.push_back(buyer);
+		record_shared_transaction(Transaction{ buyer, table_account, amount, true });
+
+		for (Account& sharer : accounts) { // Checks if account is excluded:
+			bool included = true;
+			for (Account& excluded_account : excluded_accounts)
+				if (sharer == excluded_account)
+					included = false;
+			if (included)
+				create_transaction(buyer, sharer, individual_cost); // Generates pair of transactions between buyer and sharer.
+		}
+		return true;
+	}
+	return false;
+}
+// Helpers:
 bool BalanceTable::existing_account(Account& account) {
 	// Checks if an account is on the table already.
 	for (Account& existing : accounts)
@@ -244,50 +294,33 @@ bool BalanceTable::add_account(Account& account) {
 	}
 	return false;;
 }
-bool BalanceTable::modify_balance_entry(Account& account, Account& holder, double amount) {
+bool BalanceTable::modify_balance_entry(Account& account_owner, Account& holder, double amount) {
 	// Modifies an existing account's balance with another account.
-	if (existing_account(account))
-		if (account.existing_holder(holder)) {
-			account.change_balance(holder, amount);
-			return true;
-		}
-	return false;
-}
-bool BalanceTable::create_transaction(Account& from, Account& to, double amount, bool shared) {
-	// 
-	if (existing_account(from) && existing_account(to)) {
-		Transaction t{ from, to, amount, shared };
-		// From account a to b:
-		modify_balance_entry(from, to, t.amount);
-		from.record_transaction(t);
-		// From account b to a:
-		t.amount *= -1;
-		modify_balance_entry(to, from, t.amount);
-		to.record_transaction(t);
-		return true;
-	}
-	return false;
-}
-bool BalanceTable::dist_cost(Account& buyer, double amount, vector<Account&> omissions) {
-	// Divides amount evenly amongst participating accounts:
-	double individual_cost = amount / (accounts.size() - omissions.size());
-	omissions.push_back(buyer); // Buyer already paid
-	if (existing_account(buyer)) {
-		for (Account& account : accounts) {
-
-			bool included = true;
-			for (Account& omitted_account : omissions)
-				if (omitted_account == account) included = false;
-
-			if (included) {
-				create_transaction(account, buyer, amount * -1, true);
-			}
-		}
+	if (existing_account(account_owner) && account_owner.existing_holder(holder)) {
+		account_owner.change_balance(holder, amount);
 		return true;
 	}
 	return false;
 }
 
+
+// <------ -= ======------ -= ======------> UI < ------ -= ======------ -= ======------->
+class UI {
+public:
+	UI();
+
+private:
+	string savefile;
+	BalanceTable& current_table;
+	Account& current_user;
+};
+UI::UI()
+	: savefile(default_ui_save) {
+	ifstream last_table{ savefile };
+	if (last_table) {
+		
+	}
+}
 
 /* <------ -= ======------ -= ======------> F U N C T I O N S < ------ -= ======------ -= ======------->
 	   * * * * + + + + * * * * + + + + * * * * + + <O> * * + + + + * * * * + + + + * * * * + + + +
