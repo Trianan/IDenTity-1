f03c05b391bb0557bf19535bf628338692181944
IneffablyAverage <95311691+IneffablyAverage@users.noreply.github.com>
2022-01-07 11:33:44 -0500

    Update EP_12.c

--- Project_Euler_I/EP_12.c
+++ Project_Euler_I/EP_12.c
@@ -7,8 +7,8 @@
 int main()
 {
     long long nDiv = 0;
-    long long num = 1;
-    long long adder = 2;
+    long long num = 1;         //initiallized as the first triangle number
+    long long adder = 2;       //initialized as the first number to be added to num to create the triangle sequence
 
     while (nDiv < 500) {
         nDiv = 0;
@@ -25,9 +25,9 @@ int main()
 
 
         }
-        if (nDiv < 500) {
-            num += adder;
-            adder++;
+        if (nDiv < 500) {       //if there are less than 500 divisors...
+            num += adder;       //set num to num plus adder
+            adder++;            //incriment adder
         }
     }
     printf("%lld", num);
